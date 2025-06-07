6e8ab2c1607a80697cd7196301eb03b6f64bed42
pasqualedimaro <pakydm15@gmail.com>
2025-06-07 15:29:14 +0200

    fix add preventdefault to stop reeload

--- main.js
+++ main.js
@@ -1,24 +1,9 @@
 
-
-/*const bottone = document.getElementById('bottone');
-
-bottone.addEventListener('click', function() {
-    // Prendo gli elementi
-    const inputNome = document.getElementById('nome');
-    const inputCognome = document.getElementById('cognome');
-    const inputKm = document.getElementById('km');
-    const inputEta = document.getElementById('eta');
-    
-    // Leggo i valori
-    console.log('Nome:', inputNome.value);
-    console.log('Cognome:', inputCognome.value);
-    console.log('Km:', inputKm.value);
-    console.log('Età:', inputEta.value);
-});*/
-
     const form = document.getElementById('form-biglietto');
 
     form.addEventListener('submit', function() {
+    // ipedisco al form di ricaricare la pagina
+    event.preventDefault();
     console.log('Form inviato!');
     
     const km = document.getElementById('km').value;
